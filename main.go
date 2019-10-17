be4ab22441a56f67f98411a905cac41425db25a2
Kostas Giannakakis <kgiannak1974@gmail.com>
2019-10-17 22:58:32 +0300

    Multi-insert

--- main.go
+++ main.go
@@ -11,9 +11,10 @@ import (
 
 // Anagram maps a word with its anagrams
 type Anagram struct {
-	Word     string
-	Anagrams []string
-	Count    int
+	SortedWord string
+	Word       string
+	Anagrams   []string
+	Count      int
 }
 
 func main() {
@@ -34,7 +35,13 @@ func main() {
 	wordsMap := make(map[string]int)
 	maxCount := 0
 
-	for _, word := range words {
+	var wordsToAdd []interface{}
+
+	for i, word := range words {
+
+		if i%1000 == 0 && i > 0 {
+			fmt.Printf("Handling word %d\n", i)
+		}
 
 		sortedWord := SortString(word)
 		_, exists := wordsMap[sortedWord]
@@ -46,13 +53,26 @@ func main() {
 		count := len(anagrams)
 
 		if count > 1 {
-			insertItem(collection, Anagram{Word: sortedWord, Anagrams: anagrams, Count: count})
+			wordsToAdd = append(wordsToAdd,
+				Anagram{Word: word, SortedWord: sortedWord, Anagrams: anagrams, Count: count})
+			if len(wordsToAdd) >= 100 {
+				fmt.Println("Inserting 100 words")
+				insertItems(collection, wordsToAdd)
+				wordsToAdd = nil
+			}
+
 			wordsMap[sortedWord] = count
 			if count > maxCount {
 				maxCount = count
 				fmt.Printf("Word %s: Count %d\n", word, count)
 			}
 		}
+	}
+
+	if len(wordsToAdd) > 0 {
+		fmt.Printf("Inserting %d words", len(wordsToAdd))
+		insertItems(collection, wordsToAdd)
+		wordsToAdd = nil
 	}
 
 	err = disconnect(client)
@@ -84,9 +104,17 @@ func disconnect(client *mongo.Client) (err error) {
 	return
 }
 
-func insertItem(collection *mongo.Collection, anagram Anagram) {
+func insertItem(collection *mongo.Collection, item interface{}) {
 	// Insert a single item
-	_, err := collection.InsertOne(context.TODO(), anagram)
+	_, err := collection.InsertOne(context.TODO(), item)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func insertItems(collection *mongo.Collection, items []interface{}) {
+	// Insert a single item
+	_, err := collection.InsertMany(context.TODO(), items)
 	if err != nil {
 		log.Fatal(err)
 	}
