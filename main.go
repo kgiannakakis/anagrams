0392ea4072407506afbc21e59c120cdd4a61e634
Kostas Giannakakis <kgiannak1974@gmail.com>
2019-10-19 15:39:30 +0300

    quick calculation

--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"log"
-
 	"db"
 	"db/mongo"
+	"fmt"
+	"log"
+	"time"
+
 	"words"
 )
 
 // Anagram maps a word with its anagrams
 type Anagram struct {
 	SortedWord string
-	Word       string
 	Anagrams   []string
 	Count      int
 }
@@ -24,63 +24,71 @@ func main() {
 		log.Fatal(err)
 	}
 
-	config := mongo.Config{URI: "mongodb://localhost:27017", Database: "test", Collection: "anagrams"}
+	start := time.Now()
+	wordsMap := make(map[string]Anagram)
+	maxCount := 0
+	var maxWord string
+	for _, word := range wordsList {
+		sortedWord := words.SortString(word)
+		_, exists := wordsMap[sortedWord]
+		if exists {
+			a := wordsMap[sortedWord]
+			count := a.Count + 1
+			newAnagrams := append(a.Anagrams, word)
+			wordsMap[sortedWord] = Anagram{SortedWord: sortedWord, Anagrams: newAnagrams, Count: count}
+			if count >= maxCount {
+				maxCount = count
+				maxWord = word
+				fmt.Printf("%s %d\n", word, count)
+			}
+		} else {
+			wordsMap[sortedWord] = Anagram{SortedWord: sortedWord, Anagrams: []string{word}, Count: 1}
+		}
+	}
+
+	anagrams := words.FindAnagrams(maxWord, wordsList)
+
+	for _, a := range anagrams {
+		fmt.Println(a)
+	}
+
+	elapsed := time.Since(start)
+	fmt.Printf("Took %s\n", elapsed)
+
+	start = time.Now()
+
+	var aa []interface{}
+	for _, v := range wordsMap {
+		aa = append(aa, v)
+	}
+
+	err = storeWords(aa)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	elapsed = time.Since(start)
+	fmt.Printf("Took %s\n", elapsed)
+}
+
+func storeWords(aa []interface{}) (err error) {
+	config := mongo.Config{URI: "mongodb://localhost:27017", Database: "anagrams", Collection: "anagrams"}
 
 	var saver db.DataSaver
 	saver = &mongo.Saver{}
 	err = saver.Connect(config)
 	if err != nil {
-		log.Fatal(err)
+		return
 	}
-
 	fmt.Println("Connected!")
 
-	wordsMap := make(map[string]int)
-	maxCount := 0
-
-	var wordsToAdd []interface{}
-
-	for i, word := range wordsList {
-
-		if i%1000 == 0 && i > 0 {
-			fmt.Printf("Handling word %d\n", i)
-		}
-
-		sortedWord := words.SortString(word)
-		_, exists := wordsMap[sortedWord]
-		if exists {
-			continue
-		}
-
-		anagrams := words.FindAnagrams(word, wordsList)
-		count := len(anagrams)
-
-		if count > 1 {
-			wordsToAdd = append(wordsToAdd,
-				Anagram{Word: word, SortedWord: sortedWord, Anagrams: anagrams, Count: count})
-			if len(wordsToAdd) >= 100 {
-				fmt.Println("Inserting 100 words")
-				saver.InsertMany(wordsToAdd)
-				wordsToAdd = nil
-			}
-
-			wordsMap[sortedWord] = count
-			if count > maxCount {
-				maxCount = count
-				fmt.Printf("Word %s: Count %d\n", word, count)
-			}
-		}
-	}
-
-	if len(wordsToAdd) > 0 {
-		fmt.Printf("Inserting %d words", len(wordsToAdd))
-		saver.InsertMany(wordsToAdd)
-		wordsToAdd = nil
-	}
+	saver.InsertMany(aa)
 
 	err = saver.Disconnect()
 	if err != nil {
-		log.Fatal(err)
+		return
 	}
 	fmt.Println("Connection closed.")
+
+	return
 }
